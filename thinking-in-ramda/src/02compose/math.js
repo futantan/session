9702126a3456a99539d48eefdf03e5dee374219e
futantan <luckytantanfu@gmail.com>
2018-11-28 22:57:04 +0800

    operate refactor 3

--- thinking-in-ramda/src/02compose/math.js
+++ thinking-in-ramda/src/02compose/math.js
@@ -1,12 +1,10 @@
 import * as R from "ramda";
 
-const multiply = (a, b) => a * b;
-const addOne = x => x + 1;
 const square = x => x * x;
 
 // ((x * y) + 1)^2
 export const operate = R.pipe(
-  multiply,
-  addOne,
+  R.multiply,
+  R.inc,
   square
 );
