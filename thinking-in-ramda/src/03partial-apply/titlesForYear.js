ddd60707fa181c25380c852ba7efa9610166fa3d
Tantan Fu <luckytantanfu@gmail.com>
2018-11-29 13:37:10 +0800

    partial apply refactor 6 revert order

--- thinking-in-ramda/src/03partial-apply/titlesForYear.js
+++ thinking-in-ramda/src/03partial-apply/titlesForYear.js
@@ -1,10 +1,10 @@
 // what if we want to use functions that take more than one argument?
 import * as R from "ramda";
 
-const publishedInYear = R.curry((book, year) => book.year === year);
+const publishedInYear = R.curry((year, book) => book.year === year);
 
 const titlesForYear = (books, year) => {
-  const selected = R.filter(publishedInYear(R.__, year), books);
+  const selected = R.filter(publishedInYear(year), books);
 
   return R.map(book => book.title, selected);
 };
