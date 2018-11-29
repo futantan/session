de8f262c12326a92a34f767be05f449935eba95c
futantan <luckytantanfu@gmail.com>
2018-11-29 19:44:33 +0800

    lenses | point free

--- thinking-in-ramda/src/pattern/01lenses.js
+++ thinking-in-ramda/src/pattern/01lenses.js
@@ -1,7 +1,7 @@
 // view set over
 import * as R from "ramda";
 
-export const upperUserFirstName = user => {
-  const firstName = R.lensPath(["name", "first"]);
-  return R.over(firstName, R.toUpper)(user);
-};
+export const upperUserFirstName = R.over(
+  R.lensPath(["name", "first"]),
+  R.toUpper
+);
