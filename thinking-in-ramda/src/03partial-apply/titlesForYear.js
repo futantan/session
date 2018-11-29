38b785d07bbf4e04071c915c8a403dbeff0f1909
Tantan Fu <luckytantanfu@gmail.com>
2018-11-29 13:30:56 +0800

    partial apply refactor 1

--- thinking-in-ramda/src/03partial-apply/titlesForYear.js
+++ thinking-in-ramda/src/03partial-apply/titlesForYear.js
@@ -1,10 +1,10 @@
 // what if we want to use functions that take more than one argument?
-import * as R from 'ramda';
+import * as R from "ramda";
 
-const publishedInYear = (book, year) => book.year === year;
+const publishedInYear = year => book => book.year === year;
 
 const titlesForYear = (books, year) => {
-  const selected = R.filter(book => publishedInYear(book, year), books);
+  const selected = R.filter(book => publishedInYear(year)(book), books);
 
   return R.map(book => book.title, selected);
 };
